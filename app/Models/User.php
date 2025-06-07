4b9b153fe3f928a1722ccbdd92792691027dd821
BaderHalimi <bader.halimi.2003@gmail.com>
2025-06-07 11:16:58 +0300

    install spatie library

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -52,12 +53,12 @@ class User extends Authenticatable
     {
         return $this->hasMany(Event::class);
     }
-    
+
     public function branches()
     {
         return $this->hasMany(Branch::class, 'restaurant_id');
     }
-    
+
     public function getAdditionalDataAttribute($value)
     {
         return json_decode($value, true);
