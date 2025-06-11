7ae7fc9c8377f78619d1b2f380cfbb197c11d029
BaderHalimi <bader.halimi.2003@gmail.com>
2025-06-11 11:48:43 +0300

    auth update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,24 +11,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            if ($request->is('admin') || $request->is('admin/*')) {
-                return route('admin.login');
-            }
-    
-            if ($request->is('seller') || $request->is('seller/*')) {
-                return route('seller.login');
-            }
-            if ($request->is('employee') || $request->is('employee/*')) {
-                return route('employee.login');
-            }
-            if ($request->is('checker') || $request->is('checker/*')) {
-                return route('checker.login');
-            }
-    
-    
-          
-            return route('visitor.login');
+            return redirect()->route('login');
         }
     }
-    
 }
