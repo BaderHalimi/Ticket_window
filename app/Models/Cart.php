54c85c298d73a8bb791d7a7c32b646c7e6ccc166
BaderHalimi <bader.halimi.2003@gmail.com>
2025-06-18 17:37:38 +0300

    tmp

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PDO;
 
 class Cart extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'item_id',
+        'item_type',
+        'user_id',
+        'quantity',
+        'price',
+        'discount',
+        'additional_data',
+    ];
+    public function items (){
+        return $this->morphTo();
+    }
 }
