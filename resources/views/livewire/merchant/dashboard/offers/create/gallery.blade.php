7eca197c68f729a42e6c52bd1e83a3c4f3722870
abderamaneza <astrapro54@gmail.com>
2025-06-19 19:59:51 +0100

    last relese before finishh services ss

--- resources/views/livewire/merchant/dashboard/offers/create/gallery.blade.php
+++ resources/views/livewire/merchant/dashboard/offers/create/gallery.blade.php
@@ -6,10 +6,10 @@
         <input type="file" wire:model="image" class="mb-2">
 
         @if ($offering->image)
-            <img src="{{ asset('storage/'.$offering->image) }}" class="h-32 rounded-lg shadow">
+            <img src="{{ asset('storage/'.$offering->image) ?? null}}" class="h-32 rounded-lg shadow">
         @endif
 
-        @error('image') <p class="text-red-500 text-sm">{{ $message }}</p> @enderror
+        @error('image') <p class="text-red-500 text-sm">{{ $message ?? null }}</p> @enderror
     </div>
 
     {{-- صور الجالري --}}
@@ -20,7 +20,7 @@
         <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
             @foreach ($gallery as $index => $imagePath)
                 <div class="relative group">
-                    <img src="{{ asset('storage/'.$imagePath) }}" class="w-full h-28 object-cover rounded-md shadow">
+                    <img src="{{ asset('storage/'.$index) ?? null }}" class="w-full h-28 object-cover rounded-md shadow">
                     <button wire:click="removeGalleryImage({{ $index }})" type="button"
                         class="absolute top-1 right-1 bg-red-600 text-white rounded-full w-6 h-6 text-xs hidden group-hover:flex items-center justify-center">
                         ✕
--- resources/views/merchant/dashboard/offers/create.blade.php
+++ resources/views/merchant/dashboard/offers/create.blade.php
@@ -2,33 +2,32 @@
 @section('content')
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-@php
-    $currentStep = 1;
-@endphp
 
-<div class="flex-1 p-8">
-    <div class="max-w-6xl mx-auto bg-white rounded-xl shadow-lg p-6 space-y-6">
-        <h2 class="text-2xl font-bold text-slate-800">تعديل الخدمة</h2>
 
-        <div class="grid grid-cols-12 gap-6">
+
+        {{-- <div class="grid grid-cols-12 gap-6"> --}}
             {{-- خطوات التسجيل - السلم --}}
-            <div class="col-span-3">
-                @livewire('merchant.dashboard.offers.setup-steps', ['currentStep' => $currentStep])
-            </div>
+                @livewire('merchant.dashboard.offers.setup-steps', ['offering' => $offering])
+            
 
             {{-- نموذج تعبئة المعلومات --}}
   
             
             <div class="col-span-9">
-                @livewire('merchant.dashboard.offers.create.res_settings', ['offering' => $offering])
 
-                {{-- @if ($currentStep === 1)
+             {{-- 
+
+                {{--  --}}
+
+                 {{-- @if ($currentStep == 1)
                     @livewire('merchant.dashboard.offers.create.information', ['offering' => $offering])
-                @elseif ($currentStep === 2)
-                    @livewire('merchant.dashboard.offers.steps.step-two-settings')
-                @elseif ($currentStep === 3)
-                    @livewire('merchant.dashboard.offers.steps.step-three-gallery')
-                @endif --}}
+                @elseif ($currentStep == 4)
+                    @livewire('merchant.dashboard.offers.create.gallery', ['offering' => $offering])
+                @elseif ($currentStep == 2)
+                    @livewire('merchant.dashboard.offers.create.prices', ['offering' => $offering]) 
+                @elseif ($currentStep == 3)
+                    @livewire('merchant.dashboard.offers.create.res_settings', ['offering' => $offering])
+                @endif  --}}
             </div>
 
         </div>
@@ -47,4 +46,3 @@
 </script>
 
 @endsection
-
