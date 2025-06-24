f7e7fd29e4e5e50094fbb9b6fd33cc35e2602025
abderamaneza <astrapro54@gmail.com>
2025-06-24 20:11:54 +0100

    chate table

---
+++ app/Models/Suppoer_chat.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Suppoer_chat extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'support_id',
+        'user_id',
+        'message',
+        'type',
+    ];
+}
