49d3e0af47c09af2b2f67b78e16095308ae94cf8
BaderHalimi <bader.halimi.2003@gmail.com>
2025-06-29 21:03:16 +0300

    tmp

--- routes/web.php
+++ routes/web.php
@@ -22,27 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-Route::get('/features', function () {
-    return view('features');
-})->name('features');
-Route::get('/merchant', function () {
-    return view('merchant');
-})->name('merchant');
-Route::get('/partners', function () {
-    return view('partners');
-})->name('partners');
-Route::get('/wallet', function () {
-    return view('wallet');
-})->name('wallet');
-Route::get('/roles', function () {
-    return view('roles');
-})->name('roles');
+
 Route::get('/pricing', function () {
     return view('pricing');
 })->name('pricing');
-// Route::get('/customer/dashboard', function () {
-//     return view('customer.dashboard.index');
-// })->name('customer.dashboard');
 
 Route::get('/login', function () {
     return view('auth.login');
@@ -55,17 +38,6 @@ Route::post('login', [AuthController::class, 'login'])->middleware('guest')->nam
 
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 
-// Route::get('register', [AuthController::class,'showRegisterForm'])->middleware('guest')->name('register');
-
-// Route::post('/logout', function () {
-//     auth()->logout();
-//     session()->regenerate();
-//     return redirect()->route('home')->with('success', 'You have been logged out successfully.');
-// })->middleware('auth')->name('logout');
-// // login
-// Route::get('login', [AuthController::class,'showLoginForm'])->middleware('guest')->name('login');
-// Route::post('login', [AuthController::class,'login'])->middleware('guest')->name('signin');
-// Route::post('register', [AuthController::class,'register'])->middleware('guest')->name('signup');
 Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 
 Route::get('/{id}', function ($id) {
